c9bf8f0b23d86ef8787f1529fffae1c1bb605e93
kelvincheng9938 <kelvincheng9938@gmail.com>
2025-08-26 16:36:25 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,21 @@
+import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast'
 
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#1f2937',
+            color: '#f9fafb',
+            border: '1px solid #374151',
+          },
+        }}
+      />
+    </>
+  )
+}
