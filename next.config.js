1b9277c88527a05273e6a391b768b15d9c2a8a11
kelvincheng9938 <kelvincheng9938@gmail.com>
2025-08-26 16:35:11 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  env: {
+    FMP_API_KEY: process.env.FMP_API_KEY,
+    FINNHUB_API_KEY: process.env.FINNHUB_API_KEY,
+  }
+}
 
+module.exports = nextConfig
